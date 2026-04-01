cde0fcddb1e1a8f8577a49a21ede72a77b83b195
yashmahajan01082003 <yashmahajan01082003@gmail.com>
2026-04-01 09:25:32 +0530

    Try

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import { docsConfig } from "@/config/docs";
-
 export default function Home() {
-  const firstPhase = docsConfig[0];
-  const firstItem = firstPhase.items[0];
-
-  const Component = firstItem.component;
-
-  return (
-    <div>
-      <Component />
-    </div>
-  );
+  return <h1>🔥 DEPLOY WORKING 🔥</h1>;
 }
